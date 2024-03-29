32bcd3a0fba5a8ff990e70b78d30124e7901306b
gevogevorgyan <georgegev03@gmail.com>
2024-03-29 22:22:01 +0400

    delete wrong dir

--- Examenation/route53.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_route53_record" "gg-record" {
-  zone_id = "Z10299161M47DBD68JOWT"
-  name    = "gg.com"
-  type    = "CNAME"
-  ttl     = "300"
-  records = [aws_lb.gg-alb.dns_name]
-}
